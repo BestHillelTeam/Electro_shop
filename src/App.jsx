e0cff57377d7b42bb93a1083a1b6979e15ac275d
Gvozdoedko <uraradhenko007@gmail.com>
2022-08-27 01:03:58 +0300

    fonts\styles

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import CardsTemplate from './components/CardsTemplate/CardsTemplate';
+
+function App() {
+  return (
+    <div className="App">
+      <CardsTemplate></CardsTemplate>
+    </div>
+  );
+}
+
+export default App;
