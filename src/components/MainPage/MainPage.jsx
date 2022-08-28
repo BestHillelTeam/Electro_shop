23fcbc3eb015a9987f435caf403ac8a88c6999d0
Gvozdoedko <uraradhenko007@gmail.com>
2022-08-28 17:55:50 +0300

    NavMenu and Routes V1.0

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CardsTemplate from "../CardsTemplate/CardsTemplate";
+
+export default function MainPage() {
+    return (
+        // Main page. Adding all main page elements here
+        <div>
+            <CardsTemplate />
+        </div>
+    );
+}
